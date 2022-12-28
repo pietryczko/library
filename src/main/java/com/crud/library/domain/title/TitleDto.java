441eb279d9ec7736a1485a30c127a4c9b5e36cfc
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-12-28 03:12:05 +0100

    Changes:

--- src/main/java/com/crud/library/domain/title/TitleDto.java
+++ src/main/java/com/crud/library/domain/title/TitleDto.java
@@ -1,4 +1,18 @@
 package com.crud.library.domain.title;
 
+import com.crud.library.domain.copy.Copy;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class TitleDto {
+    private int id;
+    private String title;
+    private String author;
+    private LocalDate releaseDate;
+    private List<Copy> copies;
 }
